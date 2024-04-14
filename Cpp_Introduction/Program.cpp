6c434ca268eec9d296ae13aa853df5df01daaa9d
Peter Loos <peter.loos@gmx.de>
2024-04-14 18:45:26 +0200

    typo fixed

--- Cpp_Introduction/Program.cpp
+++ Cpp_Introduction/Program.cpp
@@ -7,7 +7,7 @@
 // https://peterloos.de
 // =====================================================================================
 
-#pragma message("Collection of introductionary C++ Code Examples - Copyright (C) 2019-2024 Peter Loos")
+#pragma message("Collection of introductory C++ Code Examples - Copyright (C) 2019-2024 Peter Loos")
 
 #include <iostream>
 
@@ -38,7 +38,7 @@ extern void testExercises();
 // entry point
 int main()
 {
-    std::cout << "[Collection of introductionary C++ Code Examples - Copyright (C) 2019-2024 Peter Loos]" << std::endl;
+    std::cout << "[Collection of introductory C++ Code Examples - Copyright (C) 2019-2024 Peter Loos]" << std::endl;
 
     // main entry points code examples
     try
